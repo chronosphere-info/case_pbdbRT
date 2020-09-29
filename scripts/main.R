0386d128b56489f3f584a6fa1117f744eaf122a3
adamkocsis <adam.t.kocsis@gmail.com>
2020-09-29 12:52:42 +0200

    first content

--- scripts/main.R
+++ scripts/main.R
@@ -5,12 +5,17 @@
 # 2020-09-29
 # Erlangen 
 
+# working directory
+workDir <-file.path(Sys.getenv("WorkSpace"), "2019-07-08_chronosphere", "case_pbdbRT")
+setwd(workDir)
+
 # 1. libraries
 library(chronosphere)
 library(divDyn)
 
 # load all functions
-source()
+source("https://github.com/chronosphere-portal/case_pbdbRT/raw/master/scripts/methods/pbdb.R")
+source("https://github.com/chronosphere-portal/case_pbdbRT/raw/master/scripts/methods/plots.R")
 
 # 2. pbdb versions
 pbdbDat <- datasets("pbdb")
@@ -23,12 +28,14 @@ occsVer <- unique(occsVer)
 
 # execute function for all vers
 rtDiv <- VarFromPBDB(occsVer, var="divRT")
+saveRDS(rtDiv, file="export/rtDiv_20200929.rds")
 
 # plot the whole matrix separately
-PlotRTseparate(rtDiv, path="export/divRT/")
+data(stages)
+PlotRTcumulative(x=stages$mid, y=rtDiv, path="export/divRT/", png="divRT")
 
 # plot the whole matrix 2. on one plot
-PlotRTsingle(rtDiv)
+PlotRTsingle(x=stages$mid, y=rtDiv, path="export/", png="divRT")
 
 
 
---
+++ scripts/methods/plots.R
@@ -0,0 +1,38 @@
+# Function to plot all variable versions
+PlotRTcumulative<-function(y, x, path, png=NULL, pdf=NULL){
+	if(!is.null(png)){
+		if(!file.exists(path)) system(paste0("mkdir ", path))
+		for(i in 1:ncol(y)){
+			ver <- colnames(y)[i]
+			png(file.path(path, paste0(png, "_",ver, ".png")), width=1000, height=800)
+			Tp(ylim=c(0,8000))
+			mtext(3, text=ver)
+			for(j in 1:i) lines(x, y[,j], col="black")
+			dev.off()
+		}
+	}
+
+}
+
+
+PlotRTsingle <-function(y, x, path, png=NULL, pdf=NULL){
+	if(!is.null(png)){
+		if(!file.exists(path)) system(paste0("mkdir ", path))
+		png(file.path(path, paste0(png, "_all.png")), width=1000, height=800)
+		Tp(ylim=c(0,8000))
+		for(i in 1:ncol(y)){
+			ver <- colnames(y)[i]
+		#	mtext(3, text=ver)
+			lines(x, y[,i], col="black")
+		}
+		
+		dev.off()
+	}
+
+}
+
+Tp <- function(...){
+	data(stages, package="divDyn")
+	divDyn::tsplot(stages, boxes="sys", shading="sys",xlim=4:95, ...)
+
+}
